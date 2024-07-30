24e3d5917b2e44fdf09c8977203e262265d314d8
Acer <woeihaw.ang@atoz-software.tech>
2024-07-30 13:53:24 +0800

    change date format

--- price_api.py
+++ price_api.py
@@ -6,11 +6,11 @@ CORS(app)
 @app.route('/api/coffe', methods=['GET'])
 def get_coffe_price():
     coffe_data = pd.read_csv(
-        "C:..\\..\\web scrap\\Combine scrape price in Script(pycharm)\\webscrapingGui\\web_scraping\\data\\kopi o price.csv")
+        "C:\\Users\\Acer\\Desktop\\web_scraping\\data\\kopi o price.csv")
     coffe_data["Date"] = coffe_data["Date"].apply(lambda x: pd.to_datetime(x.replace("/", "-"), format="%d-%m-%Y"))
     coffe_date_by_month = coffe_data.groupby(pd.Grouper(key='Date', freq="M"))[
         ["Shopee Price", "Lazada Price", "PGMall Price"]].mean()
-    coffe_date_by_month.index = coffe_date_by_month.index.astype(str)
+    coffe_date_by_month.index = coffe_date_by_month.index.strftime("%m-%d-%Y")
     coffe_date_by_month = coffe_date_by_month.reset_index()
     # coffe_date_by_month = coffe_date_by_month.where(pd.notna(coffe_date_by_month), None)
     coffe_date_by_month.fillna(0,inplace = True)
